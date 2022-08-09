768ceeafb335862876f149bd6efc0b2b4376a599
João Victor <97634954+Myst1-Dev1@users.noreply.github.com>
2022-08-08 22:20:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,25 @@
-
-window.onscroll = function() {myFunction()};
-
-var header = document.getElementById("MyHeader");
-
-var sticky = header.offsetTop;
-
-function myFunction() {
-    if (window.pageYOffset > sticky) {
-      header.classList.add("sticky");
-    } else {
-      header.classList.remove("sticky");
-    }
-  }
+let menu = document.querySelector(".menu-bar");
+let header = document.querySelector(".header");
 
 
-
-  $(document).ready(function(){
-    $("#menu").click(function(){
-      $(this).toggleClass("fa-times");
-      $(".navbar").toggleClass("nav-toggle");
-    });
-});
-
-
+menu.addEventListener("click", () => {
+    header.classList.toggle("active");
+    menu.classList.toggle("fa-times");
+})
 
 // lightbox //
 
 
 document.querySelectorAll(".image-container img").forEach(image =>{
 
- // document.querySelector(".popup-image").style.display = "block";
- image.onclick = () => {
-  document.querySelector(".popup-image").style.display = "block";
-  document.querySelector(".popup-image img").src = image.getAttribute("src");
- }
-
-});
-
-document.querySelector(".popup-image span").onclick = () => {
-  document.querySelector(".popup-image").style.display = "none";
-}
+    // document.querySelector(".popup-image").style.display = "block";
+    image.onclick = () => {
+     document.querySelector(".popup-image").style.display = "block";
+     document.querySelector(".popup-image img").src = image.getAttribute("src");
+    }
+   
+   });
+   
+   document.querySelector(".popup-image span").onclick = () => {
+     document.querySelector(".popup-image").style.display = "none";
+   }
